695d6eb0fbdf75466f286eecea8d6b7813a3d2ee
Armen M <markosarmen5@gmail.com>
2025-06-26 22:22:31 -0400

    Update BookSearch.ascx.cs

--- control/BookSearch.ascx.cs
+++ control/BookSearch.ascx.cs
@@ -24,8 +24,8 @@ public partial class control_BookSearch : System.Web.UI.UserControl
         List<Book> books = new List<Book>();
         using (WebClient client = new WebClient())
         {
-            Add a user agent header in case the
-            requested URI contains a query.
+            // Add a user agent header in case the
+            // requested URI contains a query.
 
             client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
 
